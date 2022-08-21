1f671fba01c9bfc5137ba47bf968f20c78b38ad3
Sokheang <sokheangret@gmail.com>
2022-08-21 11:57:37 +0700

    add toolbar and show loading in webview

--- app/src/main/java/com/sokheang/mediaparknews/ui/article_web/ArticleWebActivity.kt
+++ app/src/main/java/com/sokheang/mediaparknews/ui/article_web/ArticleWebActivity.kt
@@ -1,9 +1,13 @@
 package com.sokheang.mediaparknews.ui.article_web
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 import com.sokheang.mediaparknews.databinding.ActivityArticleWebBinding
 import com.sokheang.mediaparknews.utils.Constants
+
 
 class ArticleWebActivity : AppCompatActivity() {
 
@@ -15,7 +19,20 @@ class ArticleWebActivity : AppCompatActivity() {
 
         val articleUrl = intent.getStringExtra(Constants.IntentConstants.ARTICLE_URL)
 
+        binding.toolbar.textClear.visibility = View.INVISIBLE
+        binding.toolbar.buttonBack.setOnClickListener {
+            onBackPressed()
+        }
+
         binding.webView.loadUrl(articleUrl!!)
+
+        binding.webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView, progress: Int) {
+                if (progress == 100) { //...page is fully loaded.
+                    binding.includeLayoutLoading.linearLoading.visibility = View.GONE
+                }
+            }
+        }
 
     }
 }
