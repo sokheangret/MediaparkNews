0bef2cca0ee93d1f3d539e3975d2e7aa48071827
Sokheang <sokheangret@gmail.com>
2022-08-21 11:08:45 +0700

    ani duplicate history save

--- app/src/main/java/com/sokheang/mediaparknews/room/data/SearchHistory.kt
+++ app/src/main/java/com/sokheang/mediaparknews/room/data/SearchHistory.kt
@@ -2,13 +2,14 @@ package com.sokheang.mediaparknews.room.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 /**
  * Create by Sokheang RET on 21-Aug-22.
  **/
 
-@Entity(tableName = "search_history")
+@Entity(tableName = "search_history", indices = [Index(value = ["history"], unique = true)])
 data class SearchHistory (
     @PrimaryKey(autoGenerate = true) val id: Int,
     @ColumnInfo(name = "history") val history: String
